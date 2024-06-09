34b9f008af67a1e7e6f81d2fdd2b4b4fac2198b3
Matt Zeller <116981276+Matt-Zeller@users.noreply.github.com>
2024-06-09 15:52:45 -0600

    Add files via upload

---
+++ 4 cts/dph51.d
@@ -0,0 +1,92 @@
+input for phases
+jb,je       0  0
+jborn       9
+nj         96 64 64 40
+sing,tr,cp  1  0  0
+iprop       1
+iphrel      1
+ipotfc
+c         1000.       0.
+wn        939.5654  938.2720
+          0.01
+          0.02
+          0.03
+          1.
+          5.
+          10.
+          25.
+          50.
+          100.
+          150.
+          200.
+          250.
+          300.
+          350.
+          400.
+          600.
+          800.
+          1000.
+elab end
+mps,analy   0  0  0  0  0  1  0
+icpp        0  0
+icoul      80
+rcoul     10.
+irma,iqua   0  0
+ipoint      0
+iwrite      2
+ipunch      0  2
+ideg        2
+N3L1   ct's only
+factor typ  1             600.
+num.int.    4 48
+nucl. mass  938.9183
+ga,fpi      1.29         92.4
+c_i       -1.07          3.20      -5.32     3.56
+d_i        1.04          -0.48      0.14     -1.90 
+e_i        
+cutall      6.                     3.        500.
+sq   opep                92.4      134.9766  0.        0.
+  
+
+
+sq   opep                92.4      139.5702  0.        0.
+
+
+
+lsj  1S0   -0.114422
+lsj  1S0    2.0
+lsj  1S0   -2.0
+lsj  1S0    0.0
+lsj  3P0  
+lsj  3P0  
+lsj  1P1  
+lsj  1P1  
+lsj  3P1  
+lsj  3P1  
+lsj  3S1 
+lsj  3S1  
+lsj  3S1  
+lsj  3S1  
+lsj  3D1  
+lsj  3SD1  
+lsj  3SD1  
+lsj  3SD1  
+lsj  1D2  
+lsj  3D2  
+lsj  3P2  
+lsj  3P2  
+lsj  3PF2  
+lsj  3D3  
+lsj  3F2
+lsj  1F3
+lsj  3F3  
+lsj  3G3
+lsj  3D-G3
+lsj  1G4  
+lsj  3G4
+lsj  3F4 
+lsj  3F-H4
+lsj  3G5
+lsj  3G-I5
+end param.
+
---
+++ 4cts+ope+2pe/dph51 N3L3.d
@@ -0,0 +1,152 @@
+input for phases
+jb,je       0  0
+jborn       9
+nj         80 64 64 40
+sing,tr,cp  1  0  0
+iprop       1
+iphrel      1
+ipotfc
+c         1000.       0.
+wn        939.5654  938.2720
+          0.01
+          0.02
+          0.03
+          1.
+          5.
+          10.
+          25.
+          50.
+          100.
+          150.
+          200.
+          250.
+          300.
+          350.
+          400.
+          500.
+          600.
+          800.
+          1000.
+elab end
+mps,analy   0  0  0  0  0  1  0
+icpp        0  0
+icoul      80
+rcoul     10.
+irma,iqua   0  0
+ipoint      0
+iwrite      2
+ipunch      0  2
+ideg        2
+N3l4        4 ct's + 1PE + 2PE(N3LO)      
+factor typ  1             600.
+num.int.    4 48
+nucl. mass  938.9183
+ga,fpi      1.29         92.4
+c_i       -1.07          3.20      -5.32     3.56
+d_i        1.04          -0.48      0.14     -1.90
+e_i      
+cutall      6.                     3.        500.
+sq   opep  -1.29         92.4      134.9766  0.        0.
+  
+
+
+sq   opep  -1.29         92.4      139.5702  0.        0.
+
+
+
+c    tpn1   1.           0.        138.0390  1.        -1.
+fun         13.
+sq   tpn1   1.           0.        138.039   0.        -1.
+fun         14.
+ss   tpn1  -1.           0.        138.039   0.        -1.
+fun         11.
+fun         14.
+c    tpn2   1.           0.        138.039   0.        -1.
+fun         57.
+sq   tpn2   1.           0.        138.0390  1.        -1.
+fun         59.
+ss   tpn2  -1.           0.        138.0390  1.        -1.
+fun         11.
+fun         59. 
+c    tpn2m  1.           0.        138.039   0.        -1.
+fun         81.
+c    tpn2m  1.           0.        138.0390  1.        -1.
+fun         82.
+sq   tpn2m  1.           0.        138.039   0.        -1.
+fun         83.
+ss   tpn2m -1.           0.        138.039   0.        -1.
+fun         11.
+fun         83.
+sq   tpn2m  1.           0.        138.0390  1.        -1.
+fun         84.
+ss   tpn2m -1.           0.        138.0390  1.        -1.
+fun         11.
+fun         84. 
+ls   tpn2m  1.           0.        138.039   0.        -1.
+fun         19.
+ls   tpn2m  1.           0.        138.0390  1.        -1.
+fun         20.
+c    tpn3   1.           0.        138.039   0.        -1.
+fun         30.
+sq   tpn3   1.           0.        138.0390  1.        -1.
+fun         31.
+ss   tpn3  -1.           0.        138.0390  1.        -1.
+fun         11.
+fun         31.
+c    tpn32  1.           0.        138.039   0.        -1.
+fun         40.
+sq   tpn32 -1.           0.        138.0390  1.        -1.
+fun         41.
+ss   tpn32  1.           0.        138.0390  1.        -1.
+fun         41.
+fun         11.
+sq   tpn32 -1.           0.        138.039   0.        -1.
+fun         42.
+ss   tpn32  1.           0.        138.039   0.        -1.
+fun         42.
+fun         11.
+c    tpn32  1.           0.        138.0390  1.        -1.
+fun         43.
+sq   tpn32 -1.           0.        138.039   0.        -1.
+fun         44.          24.
+ss   tpn32  1.           0.        138.039   0.        -1.
+fun         44.          24.
+fun         11.
+c    tpn32  1.           0.        138.039   1.        -1.
+fun         45.          24.
+lsj  1S0   0.176
+lsj  1S0   4.
+lsj  1S0  
+lsj  1S0  
+lsj  3P0  
+lsj  3P0  
+lsj  1P1  
+lsj  1P1  
+lsj  3P1  
+lsj  3P1  
+lsj  3S1 
+lsj  3S1  
+lsj  3S1  
+lsj  3S1  
+lsj  3D1  
+lsj  3SD1  
+lsj  3SD1  
+lsj  3SD1  
+lsj  1D2  
+lsj  3D2  
+lsj  3P2  
+lsj  3P2  
+lsj  3PF2  
+lsj  3D3  
+lsj  3F2
+lsj  1F3
+lsj  3F3  
+lsj  3G3
+lsj  3D-G3
+lsj  1G4  
+lsj  3G4
+lsj  3F4 
+lsj  3F-H4
+lsj  3G5
+lsj  3G-I5
+end param.
